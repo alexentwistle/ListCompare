d8c05e27da7a04b2aa900507e5ea860cc2d19823
Alex Entwistle <alexentwistleseo@gmail.com>
2019-05-08 13:06:57 +0100

    Added US version

--- smythson-uk.py
+++ smythson-uk.py
@@ -2,11 +2,11 @@
 
 # Compare two csv files. Find items that are IN FILETWO BUT NOT IN FILEONE.
 
-with open('uksitemap.csv', 'r') as t1, open('ukspider.csv', 'r') as t2:
+with open('uk-sitemap.csv', 'r') as t1, open('uk-spider.csv', 'r') as t2:
     fileone = t1.readlines()
     filetwo = t2.readlines()
 
-with open('foundbyspiderbutnotinsitemap.csv', 'w') as outFile:
+with open('uk-foundbyspiderbutnotinsitemap.csv', 'w') as outFile:
     for line in filetwo:
         if line not in fileone:
             outFile.write(line)
